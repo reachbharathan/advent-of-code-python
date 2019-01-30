bb7780359f9855c19b3534fbff81cf3a0b653aa1
Bharathan Kumaran <reachbharathan@gmail.com>
2019-01-30 20:09:18 +0530

    aoc solution 4 continue

--- advent_of_code/day_4_Repose_Record.py
+++ advent_of_code/day_4_Repose_Record.py
@@ -2,7 +2,7 @@ import re
 from collections import defaultdict
 from datetime import datetime, timedelta, time
 
-with open("/Users/Bharathan/projects/python_practise/advent_of_code/04_sample_input.txt", "r") as file:
+with open("/Users/Bharathan/projects/python_practise/advent_of_code/04_input1.txt", "r") as file:
     input1 = list(filter(None, file.read().splitlines()))
 
 print(input1)
@@ -21,9 +21,9 @@ class daywise_guard_details():
 
     def __init__(self, str=""):
         date_str = re.findall(r'\[.+\]+', str)
-        guard_str = re.findall(r'\]\s(.+)', str)
+        self.guard_str = re.findall(r'\]\s(.+)', str)
         self.set_date_time(date_str[0])
-        self.set_guard_data(guard_str[0])
+        self.set_guard_name(self.guard_str[0])
 
     def set_date_time(self, str):
         self.date_object = datetime.strptime(str, "[%Y-%m-%d %H:%M]")
@@ -42,30 +42,61 @@ class daywise_guard_details():
         print(self.date_object_str)
         print(str(str(self.date_object)))
 
-    def set_guard_data(self, guard_str="None"):
+    def set_guard_name(self, guard_str=""):
         if "Guard #" in guard_str:
             self.guard_name = re.findall(r'\d+', guard_str)[0]
+
+    def set_time_asleep(self,intvalue):
+        self.asleep_time = intvalue
 
 
 previous_guard_name = ""
 date_guard_dict = {}
+guard_name_set = set()
+guard_asleep_dict = {}
+guard_effort_fall_asleep = []
+guard_effort_wakes_up = []
+def compute_sleep_time(guard_effort_fall_asleep, guard_effort_wakes_up):
+    temp_asleep = 0
+    for i in range(len(guard_effort_fall_asleep)):
+        temp_asleep += (guard_effort_wakes_up[i] - guard_effort_fall_asleep[i])
+    return temp_asleep
+
 for x in input1:
     guard_data = daywise_guard_details(x)
-    guard_data.print_guard_time()
+    if "1518-03-09" in x:
+        print("***")
     if guard_data.guard_name and not date_guard_dict.get(guard_data.date_object):
         if guard_data.date_object.hour == 00:
-            date_guard_dict[guard_data.date_object] = guard_data.guard_name
+            # temp_date_object = datetime.combine(temp_date_object, time())
+            date_guard_dict[guard_data.guard_name]= [guard_data.date_object.strftime("%Y-%m-%d")]
         elif guard_data.date_object.hour == 23:
             temp_date_object = guard_data.date_object + timedelta(days=1)
             temp_date_object = datetime.combine(temp_date_object, time())
-            date_guard_dict[temp_date_object] = guard_data.guard_name
+            date_guard_dict[guard_data.guard_name] = [guard_data.guard_name]
+
+
+for key, value in date_guard_dict.items():
+    guard_name_set.add(value)
 
 # for x in input1:
-guard_effort_fall_asleep = []
-guard_effort_wakes_up = []
-for x in input1:
-    guard_data = daywise_guard_details(x)
-    pass
 
 for x in date_guard_dict.items():
     print(x)
+
+
+for x in guard_name_set:
+    for y in input1:
+        guard_data = daywise_guard_details(y)
+        if "727" in x:
+            print("******")
+        if date_guard_dict[guard_data.date_object_str] == x:
+            if "falls asleep" in guard_data.guard_str:
+                guard_effort_fall_asleep.append(guard_data.date_object.minute)
+            if "wakes up" in guard_data.guard_str:
+                guard_effort_wakes_up.append(guard_data.date_object.minute)
+    guard_asleep_dict[x]= compute_sleep_time(guard_effort_fall_asleep,guard_effort_wakes_up)
+
+for x,value in guard_asleep_dict.items():
+    print(x,value)
+
